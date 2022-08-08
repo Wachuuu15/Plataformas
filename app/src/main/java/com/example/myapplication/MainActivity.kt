6543e5266de72831bf1a39d0d333697ba439df27
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-08-08 12:14:28 -0600

    toast message

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.myapplication
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnToast: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btnToast = findViewById(R.id.btn_mainActivity_toast)
+        initListeners()
     }
+    private fun initListeners() {
+        btnToast.setOnClickListener {
+            Toast.makeText(applicationContext, "Jennifer Toxcon", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
