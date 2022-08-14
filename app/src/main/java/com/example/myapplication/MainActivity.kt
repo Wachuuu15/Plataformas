922f8b8ecf6c15bd7188bf5f27a0abc90c145f12
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-08-14 11:56:51 -0600

    implement URL

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,15 +11,18 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     private lateinit var btnToast: Button
     private lateinit var detalles: Button
+    private lateinit var download: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnToast = findViewById(R.id.btn_mainActivity_toast)
         detalles = findViewById(R.id.button3)
+        download = findViewById(R.id.button2)
         initListeners()
 
     }
+
     private fun initListeners() {
         btnToast.setOnClickListener {
             Toast.makeText(applicationContext, "Jennifer Toxcon", Toast.LENGTH_LONG).show()
@@ -26,6 +30,13 @@ class MainActivity : AppCompatActivity() {
 
         detalles.setOnClickListener {
             startActivity(Intent(this, MainActivity2::class.java))
+        }
+
+        download.setOnClickListener{
+            val url = "https://play.google.com/store/apps/details?id=com.whatsapp"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
         }
     }
 
