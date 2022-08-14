8c1b425099486c9bfeae04e920945574690beec6
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-08-14 11:31:06 -0600

    new activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.myapplication
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,16 +9,23 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnToast: Button
+    private lateinit var detalles: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnToast = findViewById(R.id.btn_mainActivity_toast)
+        detalles = findViewById(R.id.button3)
         initListeners()
+
     }
     private fun initListeners() {
         btnToast.setOnClickListener {
             Toast.makeText(applicationContext, "Jennifer Toxcon", Toast.LENGTH_LONG).show()
+        }
+
+        detalles.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
         }
     }
 
